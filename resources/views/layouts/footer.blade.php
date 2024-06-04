0d30439d25503d1af64d35eb8c534d267212e8e9
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2024-06-04 18:40:31 -0500

    cambios

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <div class="container-fluid bg-dark text-secondary mt-5 pt-5">
         <div class="row px-xl-5 pt-5">
             <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
-                <h5 class="text-secondary text-uppercase mb-4">Vesergen Perú</h5>
+                <h5 class="text-secondary text-uppercase mb-4">Jhonatan Fernandez</h5>
                 <p class="mb-4">No dolore ipsum accusam no lorem. Invidunt sed clita kasd clita et et dolor sed dolor. Rebum tempor no vero est magna amet no</p>
                 <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Calle chiclayo, Chiclayo</p>
                 <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@vesergenperu.com</p>
@@ -57,7 +57,7 @@
         <div class="row border-top mx-xl-5 py-4" style="border-color: rgba(256, 256, 256, .1) !important;">
             <div class="col-md-6 px-xl-0">
                 <p class="mb-md-0 text-center text-md-left text-secondary">
-                    &copy; <a class="text-primary" href="#">vesergenperu.com</a>. Todos los derechos reservados. Diseñado por
+                    &copy; <a class="text-primary" href="#">jhonatanfernandez.com</a>. Todos los derechos reservados. Diseñado por
                     <a class="text-primary" href="https://grupotyg.pe">Grupo TyG Ingenieros</a>
                 </p>
             </div>
